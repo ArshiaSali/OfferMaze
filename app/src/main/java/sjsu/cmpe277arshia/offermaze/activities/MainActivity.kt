39462f9552069115825200da1bad47e5a756fa25
Arshia Sali <arshiasali@gmail.com>
2021-03-15 18:09:25 -0700

    Added reusable custom font

--- app/src/main/java/sjsu/cmpe277arshia/offermaze/activities/MainActivity.kt
+++ app/src/main/java/sjsu/cmpe277arshia/offermaze/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package sjsu.cmpe277arshia.offermaze
+package sjsu.cmpe277arshia.offermaze.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sjsu.cmpe277arshia.offermaze.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
